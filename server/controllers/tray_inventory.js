f0715daf403c1c6c36716378ded3d66471e7660b
LWcaveman <levi_82@hotmail.com>
2022-06-23 16:22:25 -0500

    updated put request currently working

--- server/controllers/tray_inventory.js
+++ server/controllers/tray_inventory.js
@@ -1,20 +1,39 @@
 const { Crops } = require('../../db/Crops.js');
 
-let addCropToInventory = async (cropId) => {
+let addCropToInventory = async (cropId, date) => {
   return await Crops.query(`
-    INSERT INTO tray_inventory (crop_id)
-    VALUES (${cropId})
+    INSERT INTO tray_inventory (crop_id, date_planted)
+    VALUES (${cropId}, '${date}')
+  `);
+};
+
+let updateCropToInventory = async (trayId, date, cropId) => {
+  console.log('IN CONTROLLER ',trayId, date, cropId)
+  return await Crops.query(`
+    UPDATE tray_inventory 
+    SET crop_id = ${cropId},
+        date_planted ='${date}'
+    WHERE id = ${trayId}
   `);
 };
 
 let getInventory = async () => {
   return await Crops.query(`
-    SELECT id, crop_id FROM tray_inventory
+    SELECT * FROM tray_inventory
+    ORDER BY id
   `);
 };
 
+let deleteCropInventory = async (trayId) => {
+  return await Crops.query(`
+  DELETE FROM tray_inventory
+  WHERE id = ${trayId}
+`);
+};
 
 module.exports = {
   addCropToInventory,
-  getInventory
+  getInventory, 
+  updateCropToInventory,
+  deleteCropInventory 
 };
--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ const path = require('path')
 const express = require('express');
 const app = express();
 const { getAllCrops } = require('./controllers/crops_baseline.js'); 
-const { addCropToInventory, getInventory } = require('./controllers/tray_inventory.js'); 
+const { addCropToInventory, 
+        getInventory, 
+        updateCropToInventory,
+        deleteCropInventory  } = require('./controllers/tray_inventory.js'); 
+
+app.use(express.json());
 
 
 app.get('/crops', (req, res) => {
@@ -18,14 +23,31 @@ app.get('/inventory', (req, res) => {
   });
 });
 
+//the id here represents a crop id
 app.post('/tray/:id', (req, res) => {
-  addCropToInventory(req.params.id)
-  .then((data) => {
+  addCropToInventory(req.params.id, req.body.date)
+   .then((data) => {
     res.status(201).send(`updated Inventory`);
-  });
+  }).catch(err => console.log(err)); 
 });
 
+//the id here represents a tray id
+app.put('/tray/:id', (req, res) => {
+  console.log('Put: ',req.params.id, req.body.date, req.body.crop_id)
+  updateCropToInventory(req.params.id, req.body.date, req.body.crop_id)
+   .then((data) => {
+    res.status(201).send(`updated Inventory`);
+  }).catch(err => console.log(err)); 
+});
 
+//this is currently only useful in dev
+app.delete(`/tray/:id`, (req, res) => {
+  console.log('Delete: ',req.params.id)
+  deleteCropInventory(req.params.id)
+   .then((data) => {
+    res.status(201).send(`updated Inventory`);
+  }).catch(err => console.log(err)); 
+});
 
 
 
